1fd24a20dd06aa98e3b16292b63e2210d1097380
Cadenza <3696797+cdnza@users.noreply.github.com>
2023-11-28 01:54:11 -0500

    arena name case insensitivity

--- Complete Zones/Ios Landing/scripts/GameTypes/Multi/Gametypes/Coop/Coop.cs
+++ Complete Zones/Ios Landing/scripts/GameTypes/Multi/Gametypes/Coop/Coop.cs
@@ -62,8 +62,24 @@ namespace InfServer.Script.GameType_Multi
             _team = _arena.getTeamByName("Titan Militia");
             _botTeam = _arena.getTeamByName("Collective Military");
 
-            if (_arena._name.StartsWith("[Co-Op]"))
+            var arn = _arena._name.ToLower();
+
+            if (arn.StartsWith("[co-op]"))
             {   
+
+				// default difficulty is Normal
+				_botDifficulty = 1; 
+				if(!arn.EndsWith("normal")){
+					_botDifficulty = (arn.EndsWith("easy")) ? 0
+					: (arn.EndsWith("hard")?	 3:0)
+					+ (arn.EndsWith("expert")?	 6:0)
+					+ (arn.EndsWith("master")?	 9:0)
+					+ (arn.EndsWith("elite")?	12:0)
+					+ (arn.EndsWith("insane")?	15:0)
+					+ (arn.EndsWith("hell")?	35:0);
+				}
+
+				/*
                 _botDifficulty = 1;
                 if (_arena._name.EndsWith("Easy"))
                 {
@@ -100,6 +116,7 @@ namespace InfServer.Script.GameType_Multi
                 {
                     _botDifficulty = 35;
                 }
+                */
             }
         }
 
